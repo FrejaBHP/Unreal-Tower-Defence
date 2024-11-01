6c6f001e547a230ea7338af3fa272dff1a7ce3bb
Freja Pedersen <tokeahp@gmail.com>
2024-11-01 14:56:57 +0100

    Fjernet GAS fra Plugins + Build-fil

--- Source/TowerDefenceThing/TowerDefenceThing.Build.cs
+++ Source/TowerDefenceThing/TowerDefenceThing.Build.cs
@@ -2,13 +2,11 @@
 
 using UnrealBuildTool;
 
-public class TowerDefenceThing : ModuleRules
-{
-	public TowerDefenceThing(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+public class TowerDefenceThing : ModuleRules {
+    public TowerDefenceThing(ReadOnlyTargetRules Target) : base(Target) {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput", "Paper2D" });
-        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
     }
 }
